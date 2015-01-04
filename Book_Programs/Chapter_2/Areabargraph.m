3ff40a372cd7b6b5358d86336184211d669b3b7d
muvic08 <muvic08@gmail.com>
2015-01-04 11:22:56 -0500

    added areabargraph.m

--- Book_Programs/Chapter_2/Areabargraph.m
+++ Book_Programs/Chapter_2/Areabargraph.m
@@ -1,10 +1,10 @@
 #{ 
 	BOOK: 		Introduction to Probability By Charles M Grinstead and J Laurie Snell
-	CHAPTER: 	2 (Continuous Probability Densities) ~ Example 2.4
+	CHAPTER: 	2 (Continuous Probability Densities) ~ Example 2.4 & Example 2.5
 	AUTHOR: 	Victor Mutai (muvic08@gmail.com, https://github.com/muvic08)
 
 	SYNOPSIS: 
-		Suppose that we choose two random real numbers in [0, 1] and add
+		Suppose that we choose n random real numbers in [0, 1] and add
 		them together. Let X be the sum. How is X distributed?
 #}
 
@@ -20,6 +20,7 @@ end
 #{ 
 	Sample Run: 
 		ENVIRONMENT:		Octave _or Matlab	
-		COMMAND:		Areabargraph(500000,200)
+		COMMAND:		setenv("GNUTERM", "qt")  
+						Areabargraph(500000,150)
 		RESULTS:		normal distribution histogram
 #}
